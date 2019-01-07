dc6044a4f30788b8f013031c20a5130fb4b9e92f
Carlos Miguel Hudson Diaz <carlos.miguel.hudson.diaz@ibm.com>
2019-01-07 09:32:48 -0600

    Adding new changes

--- src/components/UI/SearchResults/SearchResults.js
+++ src/components/UI/SearchResults/SearchResults.js
@@ -4,19 +4,24 @@ import classes from './SearchResults.module.css';
 const SearchResults = (props) =>{
     
     const {value, pokemons} = props;
-    const filteredPokemons = pokemons.filter(pokemon => {
+    let filteredPokemons = null
+
+    filteredPokemons = pokemons.filter(pokemon => {
         return (pokemon.name.toLowerCase().indexOf(value.toLowerCase()) !== -1) ||
                 (pokemon.id.toString().toLowerCase().indexOf(value.toLowerCase()) !== -1)
     })
     return (
-        <div className={classes.SearchResults}>
-                {filteredPokemons.map(pokemon => {
-                return <div onClick={()=> props.click({pokemon})} 
-                            key={pokemon.id}>
-                        <img src={pokemon.sprites.front_default} alt="pokemon"/>
-                        <span>{pokemon.id} - {pokemon.name}</span>
-                    </div>
-                })}
+        
+        <div className={filteredPokemons.length > 0 ? classes.SearchResults : classes.Span}>
+                { filteredPokemons.length > 0 
+                    ? filteredPokemons.map(pokemon => {
+                    return <div onClick={()=> props.click({pokemon})} 
+                                key={pokemon.id}>
+                            <img src={pokemon.sprites.front_default} alt="pokemon"/>
+                            <span>{pokemon.id} - {pokemon.name}</span>
+                        </div>})
+                    : <span >There are not results for that word</span>
+                }
         </div>
     );
     
